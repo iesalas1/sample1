08af8e303c9a04fc47cbacbebe261a8c9a34317f
iesalas1 <iesalas1@asu.edu>
2019-09-12 15:42:54 -0700

    First Commit

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void yourMessage();
+
+void newFunction();
+
+void newMessage();
+
+int main()
+{
+    printf("Even newere change!!!\n");
+	printf("Hello CSE 310!\n");
+
+	printf("Create your own branch in this repository\n");
+
+	newFunction();
+
+
+	int something = 0;
+
+	something += 5;
+
+	printf("Here's something: %d\n", something);
+	
+	yourMessage();
+	newMessage();
+	
+	return 0;
+}
+
+void yourMessage()
+{
+	printf("Uncomment the line below and put in your own message\n"); 
+	
+	printf("ABCDEFGHIJKLMNOPQRSTUVWXYZ\n");
+}
+
+void newFunction()
+{
+	printf("Brand new FUNCTION!!!\n");
+}
+void newMessage()
+{
+	printf("dkfnsdlkfnalknglsdkndfklj\n");
+}
